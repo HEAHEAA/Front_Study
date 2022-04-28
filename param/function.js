79ee50b1ca7781be5b9c7671722bcd9a735a12a5
JiHee <kimjihee1113@naver.com>
2022-04-28 15:23:11 +0900

    return

---
+++ param/function.js
@@ -0,0 +1,12 @@
+// Keyword    -> function
+// Name       -> hello
+// Paramenter -> ()
+// Body       -> {}
+
+    // 선언 (매개변수 parameter)
+    // 호출 (전달인자 argument)
+
+function hello(){
+    return 'hllo';
+}
+hello();
